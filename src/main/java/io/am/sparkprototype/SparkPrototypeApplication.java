bd381632996f13a05080cadcde8d9908451eaf18
Aditya Mahaddalkar <adityam1311@gmail.com>
2023-03-01 08:46:56 -0800

    Initial Commit

---
+++ src/main/java/io/am/sparkprototype/SparkPrototypeApplication.java
@@ -0,0 +1,13 @@
+package io.am.sparkprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/am/sparkprototype/SparkPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.am.sparkprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
